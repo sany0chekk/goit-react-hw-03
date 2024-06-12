e41d38a92fa8d123e6d3d15de3c468d9d4539981
Oleksandr <olexandr.veseluy@gmail.com>
2024-06-12 21:02:43 +0300

    add clsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,15 @@
 import css from "./Contact.module.css";
 import { FaPhoneAlt, FaUser } from "react-icons/fa";
+import clsx from "clsx";
 
 const Contact = ({ id, name, number, onDelete }) => {
   return (
     <div className={css.item}>
       <div className={css.content}>
-        <p className={`${css.text} ${css.name}`}>
+        <p className={clsx(css.text, css.name)}>
           <FaUser /> {name}
         </p>
-        <p className={`${css.text} ${css.number}`}>
+        <p className={clsx(css.text, css.number)}>
           <FaPhoneAlt /> {number}
         </p>
       </div>
